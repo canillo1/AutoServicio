5a864bb638dc1d9fdd05a9f6bafdf5ff9d96c215
root <root@MASTER-LXC.home>
2026-04-20 21:24:47 +0000

    Creando proyecto.

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def inicio():
+    return "<h1>¡prueba 1  </h1>"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
