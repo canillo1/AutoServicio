2c450f5c59157e05feadfc47543120ce6620d471
root <root@MASTER-LXC.home>
2026-04-25 16:43:25 +0000

    Creando Front-end y flask actualizado

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
+app.secret_key ='KeySecreta'
 @app.route('/')
-def inicio():
-    return "<h1>¡prueba 1  </h1>"
+def index():
+    return redirect(url_for('login'))
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form.get('username')
+        password = request.form.get('password')
+        if user == "admin" and password == "admin":
+            return redirect(url_for('dashboard'))
+        else:
+            return "Error, Revisa tus credenciales."
+    return render_template('login.html')
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+@app.route('/crear_servicio')
+def crear_servicio():
+	return render_template('crear_servicio.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
